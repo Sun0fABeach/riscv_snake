3fc362494e5745673859eb23880036c13cfbae53
Markus Appel <markospoparkos@gmail.com>
2022-06-12 20:35:43 +0200

    implement division

--- lib/math.s
+++ lib/math.s
@@ -1,9 +1,11 @@
-# multiplication using the shift-add algorithm
+.global mult
+.global div
+
+# signed multiplication using the shift-add algorithm
 # a0 is multiplicand
 # a1 is multiplier
 # t0 is product
-
-.global mult
+# product placed in a0
 
 mult:
     li t0, 0
@@ -19,4 +21,27 @@ mult_noadd:
 mult_done:
     mv a0, t0
     ret
+
+
+# unsigned division using using repeated subtraction
+# performant as long as dividend and divisor aren't far apart (factor 10 is ok)
+# division by 0 results in quotient = 0, remainder = dividend
+# a0 is dividend
+# a1 is divisor
+# quotient placed in a0
+# remainder placed in a1
+
+div:
+  li t0, 0
+  beq a1, x0, div_done
+  j div_test
+div_loop:
+  sub a0, a0, a1
+  addi t0, t0, 1
+div_test:
+  bleu a1, a0, div_loop
+div_done:
+  mv a1, a0
+  mv a0, t0
+  ret
 
--- test/math.s
+++ test/math.s
@@ -1,8 +1,11 @@
 .global main
 .extern mult
+.extern div
 
 main:
     mv s11, ra
+
+#mult
     li a0, 2
     li a1, 3
     jal mult
@@ -11,6 +14,37 @@ main:
     li a1, -7
     jal mult
     mv s1, a0
+    li a0, -5
+    li a1, 7
+    jal mult
+    mv s2, a0
+    li a0, -5
+    li a1, -7
+    jal mult
+    mv s3, a0
+
+#div
+    li a0, 10
+    li a1, 5
+    jal div
+    mv s4, a0
+    mv s5, a1
+    li a0, 13
+    li a1, 6
+    jal div
+    mv s6, a0
+    mv s7, a1
+    li a0, 10
+    li a1, 12
+    jal div
+    mv s8, a0
+    mv s9, a1
+    li a0, 1
+    li a1, 0
+    jal div
+    mv t0, a0
+    mv t1, a1
+
     mv ra, s11
     ret
 
