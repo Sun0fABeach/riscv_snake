deea382a57ef5fda7dcb6213911fa7c087999185
Markus Appel <markospoparkos@gmail.com>
2022-06-28 21:09:54 +0200

    implement manual movement

---
+++ lib/state.s
@@ -0,0 +1,277 @@
+    .global move
+    .global move_up
+    .global move_right
+    .global move_down
+    .global move_left
+    .global snake_init
+
+    .extern draw_pixel
+    .extern map_init
+    .extern map_read
+    .extern map_write
+
+    .equ up, 0
+    .equ right, 1
+    .equ down, 2
+    .equ left, 3
+
+    .equ empty_color, 0
+    .equ snake_color, -1
+
+snake_init:
+    addi sp, sp, -4
+    sw ra, 0(sp)
+
+    call map_init
+
+    li a0, 15
+    li a1, 16
+    call write_tail
+    li a2, snake_color
+    call draw_pixel
+    li a0, 16
+    li a1, 16
+    li a2, snake_color
+    call draw_pixel
+    li a0, 17
+    li a1, 16
+    call write_head
+    li a2, snake_color
+    call draw_pixel
+
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    ret
+
+# a0 direction
+# returns collision 0/1 in a0
+move:
+    addi sp, sp, -4
+    sw ra, 0(sp)
+move_check_up:
+    li t0, up
+    bne a0, t0, move_check_right
+    call move_up
+    j move_done
+move_check_right:
+    li t0, right
+    bne a0, t0, move_check_down
+    call move_right
+    j move_done
+move_check_down:
+    li t0, down
+    bne a0, t0, move_check_left
+    call move_down
+    j move_done
+move_check_left:
+    li t0, left
+    bne a0, t0, move_done
+    call move_left
+move_done:
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    ret
+
+# returns collision 0/1 in a0
+move_up:
+    addi sp, sp, -12
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+# head movement
+    call read_head
+    mv s0, a0
+    mv s1, a1
+    beqz a1, move_up_collision
+    li a2, up
+    call map_write
+    addi s1, s1, -1
+    mv a0, s0
+    mv a1, s1
+    call write_head
+    li a2, snake_color
+    call draw_pixel
+# tail movement
+    call move_tail
+    li a0, 0
+    j move_up_done
+move_up_collision:
+    li a0, 1
+move_up_done:
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    addi sp, sp, 12
+    ret
+
+move_right:
+    addi sp, sp, -12
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+# head movement
+    call read_head
+    mv s0, a0
+    mv s1, a1
+    li t0, 31
+    beq a0, t0, move_right_collision
+    li a2, right
+    call map_write
+    addi s0, s0, 1
+    mv a0, s0
+    mv a1, s1
+    call write_head
+    li a2, snake_color
+    call draw_pixel
+# tail movement
+    call move_tail
+    li a0, 0
+    j move_right_done
+move_right_collision:
+    li a0, 1
+move_right_done:
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    addi sp, sp, 12
+    ret
+
+move_down:
+    addi sp, sp, -12
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+# head movement
+    call read_head
+    mv s0, a0
+    mv s1, a1
+    li t0, 31
+    beq a1, t0, move_down_collision
+    li a2, down
+    call map_write
+    addi s1, s1, 1
+    mv a0, s0
+    mv a1, s1
+    call write_head
+    li a2, snake_color
+    call draw_pixel
+# tail movement
+    call move_tail
+    li a0, 0
+    j move_down_done
+move_down_collision:
+    li a0, 1
+move_down_done:
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    addi sp, sp, 12
+    ret
+
+move_left:
+    addi sp, sp, -12
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+# head movement
+    call read_head
+    mv s0, a0
+    mv s1, a1
+    beqz a0, move_left_collision
+    li a2, left
+    call map_write
+    addi s0, s0, -1
+    mv a0, s0
+    mv a1, s1
+    call write_head
+    li a2, snake_color
+    call draw_pixel
+# tail movement
+    call move_tail
+    li a0, 0
+    j move_left_done
+move_left_collision:
+    li a0, 1
+move_left_done:
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    addi sp, sp, 12
+    ret
+
+move_tail:
+    addi sp, sp, -12
+    sw ra, 0(sp)
+    sw s0, 4(sp)
+    sw s1, 8(sp)
+
+    call read_tail
+    mv s0, a0
+    mv s1, a1
+    li a2, empty_color
+    call draw_pixel
+    mv a0, s0
+    mv a1, s1
+    call map_read
+move_tail_check_up:
+    li t0, up
+    bne a0, t0, move_tail_check_right
+    addi s1, s1, -1
+    j move_tail_write
+move_tail_check_right:
+    li t0, right
+    bne a0, t0, move_tail_check_down
+    addi s0, s0, 1
+    j move_tail_write
+move_tail_check_down:
+    li t0, down
+    bne a0, t0, move_tail_left
+    addi s1, s1, 1
+    j move_tail_write
+move_tail_left:
+    addi s0, s0, -1
+move_tail_write:
+    mv a0, s0
+    mv a1, s1
+    call write_tail
+move_tail_check_done:
+    lw ra, 0(sp)
+    lw s0, 4(sp)
+    lw s1, 8(sp)
+    addi sp, sp, 12
+    ret
+
+
+read_head:
+    la t0, head
+    lbu a0, 0(t0)
+    lbu a1, 1(t0)
+    ret
+
+read_tail:
+    la t0, tail
+    lbu a0, 0(t0)
+    lbu a1, 1(t0)
+    ret
+
+write_head:
+    la t0, head
+    sb a0, 0(t0)
+    sb a1, 1(t0)
+    ret
+
+write_tail:
+    la t0, tail
+    sb a0, 0(t0)
+    sb a1, 1(t0)
+    ret
+
+
+    .data
+# current x/y of snake head
+head:
+    .zero 2
+# current x/y of snake tail
+tail:
+    .zero 2
+
--- snake.s
+++ snake.s
@@ -1,6 +1,58 @@
-.global main
+    .global main
+    .extern snake_init
+    .extern input_init
+    .extern input_val
+    .extern move_up
+    .extern move_right
+    .extern move_down
+    .extern move_left
+
+    .equ input_up, 0x00000040
+    .equ input_right, 0x00002000
+    .equ input_down, 0x00400000
+    .equ input_left, 0x00008000
 
 main:
-  li t0, 123
-  ret
+    addi sp, sp, -4
+    sw ra, 0(sp)
+    call snake_init
+    call input_init
+main_loop:
+    la t0, input_val
+    lw a0, 0(t0)
+    beqz a0, main_loop
+    sw x0, 0(t0)
+    call main_move
+    j main_loop
 
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    ret
+
+# a0 input word
+main_move:
+    addi sp, sp, -4
+    sw ra, 0(sp)
+main_move_check_up:
+    li t0, input_up
+    bne t0, a0, main_move_check_right
+    call move_up
+    j main_move_done
+main_move_check_right:
+    li t0, input_right
+    bne t0, a0, main_move_check_down
+    call move_right
+    j main_move_done
+main_move_check_down:
+    li t0, input_down
+    bne t0, a0, main_move_check_left
+    call move_down
+    j main_move_done
+main_move_check_left:
+    li t0, input_left
+    bne t0, a0, main_move_done
+    call move_left
+main_move_done:
+    lw ra, 0(sp)
+    addi sp, sp, 4
+    ret
